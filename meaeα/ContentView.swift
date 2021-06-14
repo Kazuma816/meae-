62426f3a4a6ea2046c4f16e27161428ff42340d0
Kazuma <ktn7.tigers@outlook.jp>
2021-06-14 17:02:04 +0900

    Initial Commit

---
+++ meaeα/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  meaeα
+//
+//  Created by 櫨林一磨 on 2021/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ meaeα/meae_App.swift
@@ -0,0 +1,17 @@
+//
+//  meae_App.swift
+//  meaeα
+//
+//  Created by 櫨林一磨 on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct meae_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
