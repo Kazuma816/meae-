6181144ff4f0a8846188017be66691ae27d93b8d
Kazuma <ktn7.tigers@outlook.jp>
2021-06-20 22:22:27 +0900

    画像の中央配置と購入ボタン

--- meaeα/Inhul1.swift
+++ meaeα/Inhul1.swift
@@ -10,11 +10,30 @@ import SwiftUI
 struct Inhul1: View {
     var body: some View {
         
-        Rectangle()
+        VStack {
             
-        .fill(Color.blue)
+            Image("flo")
+                .resizable()
+                .clipShape(Circle())
+                .frame(width: 300, height: 300)
+            
+            Button(action: {
+            }, label: {
+                Text("購入する")
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.white)
+            })
+            .frame(height: 50)
+            .frame(maxWidth: .infinity)
+            .background(
+                RoundedRectangle(cornerRadius: 50, style: .continuous).fill(Color.red)
+            )
+            .overlay(
+                RoundedRectangle(cornerRadius: 50, style: .continuous)
+                    .strokeBorder(Color.blue, lineWidth: 1)
+            )
 
-        .frame(height: 161.0)
+        }
         
         
             
