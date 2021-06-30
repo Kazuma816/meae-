f59f075a1586b3ac51d1308adfc5ff10ff59d2f4
kaaa <in.kazu18@gmail.com>
2021-06-30 23:37:47 +0900

    UITabBarControllerの設置

--- meaeα/top.swift
+++ meaeα/top.swift
@@ -169,10 +169,44 @@ struct top: View {
             }
                 
             }
+            TabView {
+                        FirstView().tabItem {
+                            Text("Command")
+                            Image(systemName: "command")
+                        }
+                        SecondView().tabItem {
+                            Text("Shift")
+                            Image(systemName: "shift")
+                        }
+                        ThirdView().tabItem {
+                            Text("Option")
+                            Image(systemName: "option")
+                        }
+                    }
+                }
+            }
+
+            struct FirstView: View {
+                var body: some View {
+                    Text("タブメニュー１の画面")
+                }
+            }
+
+            struct SecondView: View {
+                var body: some View {
+                    Text("タブメニュー２の画面")
+                }
+            }
+
+            struct ThirdView: View {
+                var body: some View {
+                    Text("タブメニュー３の画面")
+                }
+            }
         }
         
-    }
-}
+
+
 
 
 
